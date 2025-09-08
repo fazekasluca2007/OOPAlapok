92d9efef9297d349b5d904f1a5c5062ce00b227a
fazekasluca2007 <fazekasl@kkszki.hu>
2025-09-08 12:13:09 +0200

    Feladat2 is ready to use

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,27 @@ namespace OOPalapok
 {
     public class Szemely
     {
-        public string nev = "Peti";
-        public int kor = 22;
+        private string nev;
+        private int kor;
+
+        public Szemely(string Nev,int Kor)
+        {
+            nev = Nev;
+            kor = Kor;
+        }
+        public string Kiir()
+        {
+            return $"A személy neve:{nev} és életkora {kor}";
+        }
+
+
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely person = new Szemely();
-            Console.WriteLine($"A személy neve:{person.nev} és életkora {person.kor}");
+            Szemely person = new Szemely("Peti",22);
+            System.Console.WriteLine(person.Kiir());
             
         }
     }
