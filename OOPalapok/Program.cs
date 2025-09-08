f55e338514d01d656fddc35f5288915ce48cee40
fazekasluca2007 <fazekasl@kkszki.hu>
2025-09-08 11:58:16 +0200

    Feladat1 is ready to use

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace OOPalapok
 {
+    public class Szemely
+    {
+        public string nev = "Peti";
+        public int kor = 22;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely person = new Szemely();
+            Console.WriteLine($"A személy neve:{person.nev} és életkora {person.kor}");
+            
         }
     }
+    
 }
