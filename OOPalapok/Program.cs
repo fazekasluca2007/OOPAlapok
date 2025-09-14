8bbf5ffbb60ead05d5bc65b6e9d9101cb0a5edcb
fazekasluca2007 <fazekasl@kkszki.hu>
2025-09-14 13:09:21 +0200

    Feladat3

---
+++ OOPalapok/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+    public class Szemely
+    {
+        private string nev;
+        private int kor;
+
+        public Szemely(string Nev,int Kor)
+        {
+            nev = Nev;
+            kor = Kor;
+        }
+        public string Kiir()
+        {
+            return $"A személy neve:{nev} és életkora {kor}";
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Szemely person = new Szemely("Peti",22);
+            System.Console.WriteLine(person.Kiir());
+            
+        }
+    }
+    
+}
