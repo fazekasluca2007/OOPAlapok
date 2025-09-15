89e0582aa141ae8c58fb0ee771dd41622413cdab
fazekasluca2007 <fazekasl@kkszki.hu>
2025-09-15 09:13:47 +0200

    Feladat4 is ready to use

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -9,7 +9,7 @@ namespace OOPalapok
 {
     public class Szemely
     {
-<<<<<<< HEAD
+
         private string _nev;
         private int _kor;
 
@@ -30,62 +30,26 @@ namespace OOPalapok
                     Console.WriteLine("Nem lehet negatív.");
             }
         }
-        /*public Szemely(string Nev,int Kor)
-=======
-        private string nev;
-        private int kor;
-
-        public Szemely(string Nev,int Kor)
->>>>>>> b697acaf912ea2052c789e7a9af8d1ec6dfea628
+      
+       public override string ToString()
         {
-            nev = Nev;
-            kor = Kor;
+            return $"A személy neve: {_nev} és az életkora {_kor}";
         }
-<<<<<<< HEAD
-        */
-
-
-=======
-        public int Kor
-        {
-            get { return kor; }
-            set
-            {
-                if (value < 0)
-                {
-                    Console.WriteLine("Hiba: Az életkor nem lehet negatív!");
-                }
-                else
-                {
-                    kor = value;
-                }
-            }
-        }
->>>>>>> b697acaf912ea2052c789e7a9af8d1ec6dfea628
-        public string Kiir()
-        {
-            return $"A személy neve:{nev} és életkora {kor}";
-        }
-
-
+        
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Szemely person = new Szemely();
             person.Nev = "Kiss Ilona";
             person.Kor = 23;
-            Console.WriteLine($"A személy neve:{person.Nev} és {person.Kor} éves");
+            Console.WriteLine(person);
 
            /* Szemely person = new Szemely("Peti",22);
             System.Console.WriteLine(person.Kiir());*/
-=======
-            Szemely person = new Szemely("Peti",22);
-            System.Console.WriteLine(person.Kiir());
 
->>>>>>> b697acaf912ea2052c789e7a9af8d1ec6dfea628
             
         }
     }
